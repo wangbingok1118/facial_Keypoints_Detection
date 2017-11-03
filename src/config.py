71470fdf76df58efb25167f590278a9aadaa7c4c
wangbing <wangbingok1118@163.com>
2017-11-03 15:53:58 +0800

    add file

--- src/config.py
+++ src/config.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 
+
+import os
+import sys
+import pandas as pd
+import numpy as np
+
+
 """
 configure
 """
--- src/dataProcess.py
+++ src/dataProcess.py
@@ -1,4 +1,28 @@
 # -*- coding: utf-8 -*-
+import config
+import numpy as np
+import os
+import sys
+import pandas as pd
+
+
+def loadData(fileName=None,test=False):
+    """
+    the function used to read csv file
+    :param fileName:
+    :param test:
+    :return:
+    """
+    df = pd.read_csv(fileName)  # load pandas dataframe
+    df['Image'] = df['Image'].apply(lambda img : np.fromstring(img,sep=' '))
+    print('%s describe info is :'%(fileName))
+    print(df.count())
+    df.dropna()
+
+    X = np.vstack(df['Image'].values) / 255.  # scale pixel values to [0, 1]
+    X = X.astype(np.float32)
+
+    pass
 
 def main():
     pass
